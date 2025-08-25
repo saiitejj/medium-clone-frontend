18472754237a4eec2055dce397a4f7925c393006
Sai Tej <ngvst2004@gmail.com>
2025-08-25 13:27:17 +0530

    Pages : Fixed Register to login page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,10 @@
 import {useState} from 'react';
 import axios from 'axios';
 import './Form.css'
+import { useNavigate } from 'react-router-dom';
 function RegisterPage(){
     // return <h1>Register Page</h1>
+    const navigate=useNavigate();
     const [formData,setFormData]=useState({
         name:'',
         email:'',
@@ -21,6 +23,8 @@ function RegisterPage(){
             const response=await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/auth/register`,formData)
             console.log('Registration successful:',response.data)
             alert('Registration Successful!')
+            navigate(`/login`)
+            
         }catch(err){
             console.error('There was an error registering:',err.response.data);
             alert('Error: '+err.response.data.message);
